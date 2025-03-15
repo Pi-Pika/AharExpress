63a652b33a57e84aa3dd7c6072ec9e6687f210c0
Pi-Pika <piash15-5606@diu.edu.bd>
2025-03-15 09:38:55 +0600

    "29.35 my tab bar"

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,9 @@
+import 'package:first_app/components/my_current_location.dart';
+import 'package:first_app/components/my_description_box.dart';
 import 'package:flutter/material.dart';
 
 import '../components/my_drawer.dart';
+import 'package:first_app/components/my_silver_app_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,8 +16,31 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home"),),
       drawer: MyDrawer(),
+      body: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxScrolled) => [
+          MySliverAppBar(
+            title: Text("Title"),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Divider(
+                  indent: 25,
+                  endIndent: 25,
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+
+                // my current location
+                MyCurrentLocation(),
+
+                //descriptionbox
+                MyDescriptionBox(),
+              ],
+            ),
+            ),
+          ],
+          body: Container(color: Colors.blue),
+      ),
     );
   }
 }
