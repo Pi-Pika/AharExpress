a6cf321499197502a9bda6236cbd36bed7203330
Pi-Pika <piash15-5606@diu.edu.bd>
2025-03-16 18:38:25 +0600

    47.07 food page

--- lib/components/my_food_tile.dart
+++ lib/components/my_food_tile.dart
@@ -17,23 +17,50 @@ class FoodTile extends StatelessWidget {
       children: [
         GestureDetector(
           onTap: onTap,
-          child: Row(
-            children: [
-              //text food details
-              Expanded(
-                child: Column(
-                  children: [
-                    Text(food.name),
-                    Text(food.price.toString()),
-                    Text(food.description),
-                  ],
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Row(
+              children: [
+                //text food details
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(food.name),
+                      Text(
+                          '\$' + food.price.toString(),
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                          food.description,
+                          style: TextStyle(
+                          color:  Theme.of(context).colorScheme.inversePrimary,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
 
-              //food Image
-              Image.asset(food.imagePath, height: 120),
-            ],
+                const SizedBox(width: 15),
+
+                //food Image
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.asset(food.imagePath, height: 120),
+                ),
+              ],
+            ),
           ),
+        ),
+
+        // divider line
+        Divider(
+          color: Theme.of(context).colorScheme.tertiary,
+          endIndent: 25,
+          indent: 25,
         ),
       ],
     );
