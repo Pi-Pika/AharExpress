e890efc6531721c99efc896eadd885754931e960
Pi-Pika <piash15-5606@diu.edu.bd>
2025-03-18 14:06:04 +0600

    1.06.15 my cart tile

--- lib/model/restaurant.dart
+++ lib/model/restaurant.dart
@@ -3,14 +3,14 @@ import 'package:first_app/model/cart_item.dart';
 import 'package:flutter/cupertino.dart';
 import 'food.dart';
 
-class Restaurant extends ChangeNotifier{
-
+class Restaurant extends ChangeNotifier {
   //list of food menu
   final List<Food> _menu = [
     //burgers
     Food(
       name: "Classic CheeseBurger",
-      description: "A Juicy beef patty with melted cheddar, lettuce, tomato, and  a hint of onion and pickle.",
+      description:
+          "A Juicy beef patty with melted cheddar, lettuce, tomato, and  a hint of onion and pickle.",
       imagePath: "lib/images/burgers/cheese_burger.jpeg",
       price: 0.99,
       category: FoodCategory.Burgers,
@@ -22,7 +22,8 @@ class Restaurant extends ChangeNotifier{
     ),
     Food(
       name: "Aloha Burger",
-      description: "Plump and juicy beef patties, grilled pineapples and a special creamy burger sauce, these Aloha Burgers are extra special and delicious!",
+      description:
+          "Plump and juicy beef patties, grilled pineapples and a special creamy burger sauce, these Aloha Burgers are extra special and delicious!",
       imagePath: "lib/images/burgers/aloha_burger.jpg",
       price: 1.52,
       category: FoodCategory.Burgers,
@@ -34,7 +35,8 @@ class Restaurant extends ChangeNotifier{
     ),
     Food(
       name: "BBQ Burger",
-      description: "These BBQ Burgers are an amazingly juicy explosion of sweet, tangy, spicy, smokey, crispy deliciousness all at the same time.",
+      description:
+          "These BBQ Burgers are an amazingly juicy explosion of sweet, tangy, spicy, smokey, crispy deliciousness all at the same time.",
       imagePath: "lib/images/burgers/bbq_burger.jpg",
       price: 2.52,
       category: FoodCategory.Burgers,
@@ -46,7 +48,8 @@ class Restaurant extends ChangeNotifier{
     ),
     Food(
       name: "Bluemoon Burger",
-      description: "Blue Moon Burgers uses premium PNW sourced beef to deliver the best tasting burger around.",
+      description:
+          "Blue Moon Burgers uses premium PNW sourced beef to deliver the best tasting burger around.",
       imagePath: "lib/images/burgers/bluemoon_burger.jpeg",
       price: 3.52,
       category: FoodCategory.Burgers,
@@ -58,7 +61,8 @@ class Restaurant extends ChangeNotifier{
     ),
     Food(
       name: "Veggie Burger",
-      description: "A veggie burger or meatless burger is a hamburger made with a patty that does not contain meat, or the patty of such a hamburger. ",
+      description:
+          "A veggie burger or meatless burger is a hamburger made with a patty that does not contain meat, or the patty of such a hamburger. ",
       imagePath: "lib/images/burgers/vege_burger.jpg",
       price: 0.99,
       category: FoodCategory.Burgers,
@@ -69,11 +73,11 @@ class Restaurant extends ChangeNotifier{
       ],
     ),
 
-
     //salad
     Food(
       name: "Quinoa Salad",
-      description: "This quinoa salad recipe is the best! Everyone loves this healthy quinoa salad made with quinoa, chickpeas, red bell pepper, cucumber, parsley and lemon.",
+      description:
+          "This quinoa salad recipe is the best! Everyone loves this healthy quinoa salad made with quinoa, chickpeas, red bell pepper, cucumber, parsley and lemon.",
       imagePath: "lib/images/salads/quinoa_salad.jpeg",
       price: 1.99,
       category: FoodCategory.Salads,
@@ -85,7 +89,8 @@ class Restaurant extends ChangeNotifier{
     ),
     Food(
       name: "Asian Sesame Salad",
-      description: "This Asian Sesame Chicken Salad is a great way to secretly pack your family’s diet with a ton of vegetables. Hearty cabbage and greens are topped off with cooked chicken and crunchy almonds, then tossed in an Asian sesame ginger dressing.",
+      description:
+          "This Asian Sesame Chicken Salad is a great way to secretly pack your family’s diet with a ton of vegetables. Hearty cabbage and greens are topped off with cooked chicken and crunchy almonds, then tossed in an Asian sesame ginger dressing.",
       imagePath: "lib/images/salads/asiansesame_salad.jpeg",
       price: 1.50,
       category: FoodCategory.Salads,
@@ -97,7 +102,8 @@ class Restaurant extends ChangeNotifier{
     ),
     Food(
       name: "Caesar Salad",
-      description: "This classic Caesar salad recipe features crisp romaine, crunchy croutons, and Parmesan cheese. A creamy, tangy homemade dressing ties it together.",
+      description:
+          "This classic Caesar salad recipe features crisp romaine, crunchy croutons, and Parmesan cheese. A creamy, tangy homemade dressing ties it together.",
       imagePath: "lib/images/salads/ceasar_salad.jpeg",
       price: 1.20,
       category: FoodCategory.Salads,
@@ -109,7 +115,8 @@ class Restaurant extends ChangeNotifier{
     ),
     Food(
       name: "Greek Salad",
-      description: "This easy Greek salad recipe is our new favorite summer side dish! Just 7 ingredients + a simple Greek salad dressing make it refreshing & delicious.",
+      description:
+          "This easy Greek salad recipe is our new favorite summer side dish! Just 7 ingredients + a simple Greek salad dressing make it refreshing & delicious.",
       imagePath: "lib/images/salads/greek_salad.jpeg",
       price: 1.20,
       category: FoodCategory.Salads,
@@ -121,7 +128,8 @@ class Restaurant extends ChangeNotifier{
     ),
     Food(
       name: "Southwesst Salad",
-      description: "This Southwest Salad recipe has quickly become one of my most popular and most pinned recipes of all time.  It’s fresh, filling, flavorful, gluten-free and will have you actually CRAVING salad!",
+      description:
+          "This Southwest Salad recipe has quickly become one of my most popular and most pinned recipes of all time.  It’s fresh, filling, flavorful, gluten-free and will have you actually CRAVING salad!",
       imagePath: "lib/images/salads/southwest_salad.jpg",
       price: 1.20,
       category: FoodCategory.Salads,
@@ -131,12 +139,19 @@ class Restaurant extends ChangeNotifier{
         Addon(name: " ", price: 0.00),
       ],
     ),
+
+    // sides
+
+    //deserts
+
+    //drinks
   ];
   /*
   G E T T E R S
   */
 
   List<Food> get menu => _menu;
+  List<CartItem> get cart => _cart;
 
   /*
   O P R E R A T I O N S
@@ -168,8 +183,8 @@ class Restaurant extends ChangeNotifier{
     else {
       _cart.add(
         CartItem(
-            food: food,
-            selectedAddons: selectedAddons,
+          food: food,
+          selectedAddons: selectedAddons,
         ),
       );
     }
