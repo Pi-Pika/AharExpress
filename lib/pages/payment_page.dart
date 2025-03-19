02c722cae67a3a9ff0bffad5e9bf85ee7b38fca8
Pi-Pika <piash15-5606@diu.edu.bd>
2025-03-19 23:40:29 +0600

    1.23.11 my_receipt

--- lib/pages/payment_page.dart
+++ lib/pages/payment_page.dart
@@ -1,3 +1,5 @@
+import 'package:first_app/components/my_button.dart';
+import 'package:first_app/pages/delivery_progress_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_credit_card/flutter_credit_card.dart';
 
@@ -9,11 +11,58 @@ class PaymentPage extends StatefulWidget {
 }
 
 class _PaymentPageState extends State<PaymentPage> {
-  String cardNumber = " ";
-  String expiryDate = " ";
-  String cardHolderName = " ";
-  String cvvCode = " ";
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  String cardNumber = '';
+  String expiryDate = '';
+  String cardHolderName = '';
+  String cvvCode = '';
   bool isCvvFocused = false;
+
+  //user wants to pay
+  void userTappedPay() {
+    if (formKey.currentState!.validate()) {
+      //only show dialog if form is valid
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text("Confirm Payment"),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: [
+                Text("Card Number: $cardNumber"),
+                Text("Expiry Date: $expiryDate"),
+                Text("Card Holder Number: $cardHolderName"),
+                Text("CVV: $cvvCode"),
+              ],
+            ),
+          ),
+          actions: [
+            //cancel button
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text("Cancel"),
+            ),
+
+            //yes button
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DeliveryProgressPage(),
+                  ),
+                );
+              },
+              child: const Text("Yes"),
+            ),
+          ],
+        ),
+      );
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,21 +72,48 @@ class _PaymentPageState extends State<PaymentPage> {
         foregroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Checkout"),
       ),
-      body: Column(
-        children: [
-          //credit card
-          CreditCardWidget(
-
-            cardNumber: cardNumber,
-            expiryDate: expiryDate,
-            cardHolderName: cardHolderName,
-            cvvCode: cvvCode,
-            showBackView: isCvvFocused,
-            onCreditCardWidgetChange: (p0) {},
-          ),
-
-          //credit card form
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //credit card
+            CreditCardWidget(
+        
+              cardNumber: cardNumber,
+              expiryDate: expiryDate,
+              cardHolderName: cardHolderName,
+              cvvCode: cvvCode,
+              showBackView: isCvvFocused,
+              onCreditCardWidgetChange: (p0) {},
+            ),
+        
+            //credit card form
+            CreditCardForm(
+                cardNumber: cardNumber,
+                expiryDate: expiryDate,
+                cardHolderName: cardHolderName,
+                cvvCode: cvvCode,
+                onCreditCardModelChange: (data){
+                  setState(() {
+                    cardNumber = data.cardNumber;
+                    expiryDate = data.expiryDate;
+                    cardHolderName = data.cardHolderName;
+                    cvvCode = data.cvvCode;
+                  });
+                },
+                formKey: formKey,
+            ),
+        
+            //const Spacer(),
+        
+            MyButton(
+                text: "Pay Now",
+                onTap: userTappedPay,
+            ),
+        
+            const SizedBox(height: 25),
+        
+          ],
+        ),
       ),
     );
   }
