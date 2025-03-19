dbe18c75d36bb170be63e5789de640bcea86e98b
Pi-Pika <piash15-5606@diu.edu.bd>
2025-03-19 09:39:24 +0600

    1.15.53 Clear Cart done

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:first_app/model/restaurant.dart';
 
+import '../components/my_cart_tile.dart';
+
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
@@ -15,9 +17,39 @@ class CartPage extends StatelessWidget {
       //scaffodl UI
       return Scaffold(
         appBar: AppBar(
-          title: Text("Cart"),
+          title: const Text("Cart"),
           backgroundColor: Colors.transparent,
           foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+          actions: [
+            // clear cart button
+            IconButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Text("Are you sure you want to clear the cart?"),
+                    actions: [
+                      //cancel button
+                      TextButton(
+                        onPressed: () => Navigator.pop(context),
+                        child: const Text("Cancel"),
+                      ),
+
+                      //confirm button
+                      TextButton(
+                        onPressed: () {
+                          restaurant.clearCart();
+                          Navigator.pop(context);
+                        },
+                        child: const Text("Confirm"),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              icon: const Icon(Icons.delete),
+            ),
+          ],
         ),
         body: Column(
           children: [
@@ -29,8 +61,8 @@ class CartPage extends StatelessWidget {
                     final cartItem = userCart[index];
 
                     //return cart tile item
-                    return ListTile(
-                      title: Text(cartItem.food.name),
+                    return MyCartTile(
+                      cartItem: cartItem,
                     );
                   }),
             ),
