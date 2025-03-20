230a5e247c84977698b2b9a8b332a66ccd432f61
Pi-Pika <piash15-5606@diu.edu.bd>
2025-03-21 00:26:57 +0600

    1.30.13 driver pro pic

--- lib/model/restaurant.dart
+++ lib/model/restaurant.dart
@@ -1,6 +1,7 @@
 import 'package:collection/collection.dart';
 import 'package:first_app/model/cart_item.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:intl/intl.dart';
 import 'food.dart';
 
 class Restaurant extends ChangeNotifier {
@@ -244,8 +245,48 @@ class Restaurant extends ChangeNotifier {
   */
 
   // generate a receipt
+  String displayCartReceipt() {
+    final receipt = StringBuffer();
+    receipt.writeln("Here is your receipt.");
+    receipt.writeln();
+
+    // Date format Second porjonto update dibe
+    String formattedDate =
+      DateFormat("yyyy-MM-dd HH:mm:ss").format(DateTime.now());
+
+    receipt.writeln(formattedDate);
+    receipt.writeln();
+    receipt.writeln("------------");
+
+    for (final cartItem in _cart) {
+      receipt.writeln(
+          "${cartItem.quantity} x ${cartItem.food.name} - ${_formatPrice(
+              cartItem.food.price)}"
+      );
+      if (cartItem.selectedAddons.isNotEmpty) {
+        receipt
+            .writeln("  Add-ons: ${_formatAddons(cartItem.selectedAddons)}");
+      }
+      receipt.writeln();
+    }
+
+    receipt.writeln("--------------");
+    receipt.writeln();
+    receipt.writeln("Total Items: ${getTotalItamCount()}");
+    receipt.writeln("Total Price: ${_formatPrice(getTotalPrice())}");
+
+    return receipt.toString();
+  }
 
   // format double value into money
+  String _formatPrice(double price) {
+    return "Tk.${price.toStringAsFixed(2)}";
+  }
 
   //format list of addons into a string summary
+  String _formatAddons(List<Addon> addons) {
+      return addons
+          .map((addon) => "${addon.name} (${_formatPrice(addon.price)})")
+          .join(", ");
+  }
 }
--- lib/pages/delivery_progress_page.dart
+++ lib/pages/delivery_progress_page.dart
@@ -1,3 +1,4 @@
+import 'package:first_app/components/my_receipt.dart';
 import 'package:flutter/material.dart';
 
 class DeliveryProgressPage extends StatelessWidget {
@@ -9,6 +10,36 @@ class DeliveryProgressPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Delivery in Progress"),
         backgroundColor: Colors.transparent,
+      ),
+      bottomNavigationBar: _buildBottomNavBar(context),
+      body: Column(
+        children: [
+          MyReceipt(),
+        ],
+      ),
+    );
+  }
+
+  // Custom bottom Nav Bar
+  Widget _buildBottomNavBar(BuildContext context) {
+    return Container(
+      height: 100,
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.secondary,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(40),
+          topRight: Radius.circular(40),
+        ),
+      ),
+      child: Row(
+        children: [
+          // driver er profile pic
+          Container(
+            decoration: BoxDecoration(
+              color:
+            ),
+          )
+        ],
       ),
     );
   }
